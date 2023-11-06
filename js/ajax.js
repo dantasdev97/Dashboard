15a7574a9744774c44aba47136fc5f7703d0b7ff
dantasdev97 <144571833+dantasdev97@users.noreply.github.com>
2023-11-06 13:03:24 +0000

    .

--- js/ajax.js
+++ js/ajax.js
@@ -3,16 +3,19 @@ let content = document.getElementById('ajax-content');
 function fetchContent(el) {
     let view = el.getAttribute('a-view');
     let folder = el.getAttribute('a-folder');
-    
-    fetch(`/ajax/${folder} ${view}.html`)
-        .then(response => response.text()) // Aqui, esperamos pela resolução da promessa de texto
+
+    fetch(`/ajax/${folder}/${view}.html`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
         .then(html => {
-            content.innerHTML = html;
+            console.log(html); // Exibe o conteúdo HTML no console (opcional)
+            content.innerHTML = html; // Define o conteúdo da div com o ID 'ajax-content'
         })
         .catch(error => {
-            console.error('Erro ao buscar conteúdo:', error);
+            console.error('Error:', error);
         });
 }
-
-let button = document.querySelector('form button.btn')
-button,addEventListener
